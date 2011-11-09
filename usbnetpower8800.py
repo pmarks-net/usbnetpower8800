f577aef36b47053f9739782e8f35a602f36fc7c5
Paul Marks <paul@pmarks.net>
2011-11-09 18:39:01 +0000

    Contributions from Jonathan Dixon: - Add a 'query' command to report the current state - Move the work into a main() function so the module can be more easily   imported into other python projects - Added some setup tips
    Plus some tweaks of my own:
    - Wrap the controller into a class, so the caller can decide how long
      to keep the device open.
    - Add some exit codes.

--- usbnetpower8800.py
+++ usbnetpower8800.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 # Copyright (C) 2011  Paul Marks  http://www.pmarks.net/
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,8 @@
 # limitations under the License.
 
 
+# Project homepage: http://code.google.com/p/usbnetpower8800/
+#
 # This is a simple command-line tool for controlling the "USB Net Power 8800"
 # from Linux (etc.) using Python and PyUSB.  It shows up under lsusb as:
 #
@@ -27,6 +29,15 @@
 # The device is basically a box with a USB port and a switchable power outlet.
 # It has the unfortunate property that disconnecting it from USB immediately
 # kills the power, which reduces its usefulness.
+#
+# To install Python and usb.core on Ubuntu:
+#  $ sudo apt-get install python python-setuptools
+#  $ sudo easy_install pyusb
+#
+# If you have a permission error using the script and udev is used on your
+# system, it can be used to apply the correct permissions. Example:
+#  $ cat /etc/udev/rules.d/51-usbpower.rules
+#  SUBSYSTEM=="usb", ATTR{idVendor}=="067b", MODE="0666", GROUP="plugdev"
 
 
 import sys
@@ -34,33 +45,51 @@ import usb.core
 
 usage = (
     "Controller for the USB Net Power 8800\n"
-    "Usage: %s on|off|toggle\n")
+    "Usage: %s on|off|toggle|query\n")
 
-def IsPowerOn():
-    # Return True if the power is currently switched on.
-    ret = dev.ctrl_transfer(0xc0, 0x01, 0x0081, 0x0000, 0x0001)
-    return ret[0] == 0xa0
 
-def SetPower(on):
-    # If True, turn the power on, else turn it off.
-    code = 0xa0 if on else 0x20
-    dev.ctrl_transfer(0x40, 0x01, 0x0001, code, [])
+class Power(object):
+    def __init__(self):
+        # Find the device.
+        self.dev = usb.core.find(idVendor=0x067b, idProduct=0x2303)
+        if self.dev is None:
+            raise ValueError("Device not found")
 
-# Find the device.
-dev = usb.core.find(idVendor=0x067b, idProduct=0x2303)
-if dev is None:
-    raise ValueError("Device not found")
+    def IsOn(self):
+        # Return True if the power is currently switched on.
+        ret = self.dev.ctrl_transfer(0xc0, 0x01, 0x0081, 0x0000, 0x0001)
+        return ret[0] == 0xa0
 
-try:
-    cmd = sys.argv[1].lower()
-except IndexError:
-    cmd = ""
+    def Set(self, on):
+        # If True, turn the power on, else turn it off.
+        code = 0xa0 if on else 0x20
+        self.dev.ctrl_transfer(0x40, 0x01, 0x0001, code, [])
 
-if cmd == "on":
-    SetPower(True)
-elif cmd == "off":
-    SetPower(False)
-elif cmd == "toggle":
-    SetPower(not IsPowerOn())
-else:
-    sys.stdout.write(usage % sys.argv[0])
+
+def main(argv):
+    try:
+        cmd = argv[1].lower()
+    except IndexError:
+        cmd = ""
+
+    power = Power()
+
+    if cmd == "on":
+        power.Set(True)
+    elif cmd == "off":
+        power.Set(False)
+    elif cmd == "toggle":
+        power.Set(not power.IsOn())
+    elif cmd == "query":
+        on = power.IsOn()
+        sys.stdout.write("Power: %s\n" % ("on" if on else "off"))
+        return 0 if on else 1
+    else:
+        sys.stdout.write(usage % argv[0])
+        return -1
+    return 0
+
+
+if '__main__' == __name__:
+    ret = main(sys.argv)
+    sys.exit(ret)
